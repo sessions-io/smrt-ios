d3c89d3c58aeb7d838a10d2c40d530c5ea68a1ae
Jeff Kingyens <jeff.kingyens@gmail.com>
2016-01-18 11:22:03 -0800

    view controllers

---
+++ ChallengeView/ActiveChallengesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActiveChallengesViewController.h
+//  ChallengeView
+//
+//  Created by Jeff Kingyens on 1/18/16.
+//  Copyright © 2016 Sessions-io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActiveChallengesViewController : UITableViewController
+
+@end
---
+++ ChallengeView/SavedChallengesViewController.h
@@ -0,0 +1,13 @@
+//
+//  SavedChallengesViewController.h
+//  ChallengeView
+//
+//  Created by Jeff Kingyens on 1/18/16.
+//  Copyright © 2016 Sessions-io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SavedChallengesViewController : UITableViewController
+
+@end
